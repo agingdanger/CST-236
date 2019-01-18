d4bb3b34c75ae5d2debeed19909a72968921d203
Charles Henderson <chenderson33@my.gcu.edu>
2019-01-17 17:40:19 -0700

    Made changes to the dbconnector file

--- db_connector.php
+++ db_connector.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
-$username = "root";
-$password = "@nj562hj3k9!";
-$dbname = "CST236Login";
+$servername = "tuy8t6uuvh43khkk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "cfxok5ub03el74fe";
+$password = "gdfr6l2jjh74yy91";
+$dbname = "l426moc0o088s6g9";
 
 // Create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);
