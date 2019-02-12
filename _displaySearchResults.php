1f02c170bb66d925283be017f9c8feb7e0af1229
Charles Henderson <chenderson33@my.gcu.edu>
2019-02-11 17:46:49 -0700

    it worked! now formatting the table information to look better

--- _displaySearchResults.php
+++ _displaySearchResults.php
@@ -1,17 +1,43 @@
-<?php 
-// Expects an array of $person. Display the results in a table.
-//require_once 'SearchHandler.php';
-//person array
-
-//print_r($persons);
-
-for($x = 0; $x < count($products); $x++){
-    echo $products[$x]['PID'] . "     ";
-    echo $products[$x]['PName'] .  "     ";
-    echo $products[$x]['PDescription'] .  "     ";
-    echo $products[$x]['PPrice'] .  "     ";
-    echo "<br>";
-    
-}
-
-?>
+<html>
+	<head>
+		<meta charset="UTF-8"> <!-- text encoding set up-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--viewport defined to scale for all devices-->
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"> <!--backwards compatabillity with IE/edge products-->
+		<title> Login </title> <!-- tab title display -->
+	</head>
+	<body>
+		<table>
+			<td>
+				ID
+			</td>
+			<td>
+				Product Name
+			</td>
+			<td>
+				Product Department
+			</td>
+			<td>
+				Product Price
+			</td>
+				
+		<?php 
+            // Expects an array of $person. Display the results in a table.
+            //require_once 'SearchHandler.php';
+            //person array
+            
+            //print_r($persons);
+            
+            for($x = 0; $x < count($products); $x++)
+            {
+                echo "<tr>";
+                echo "<td>" . $products[$x]['PID'] . "<td>";
+                echo "<td>" . $products[$x]['PName'] . "<td>";
+                echo "<td>" . $products[$x]['PDescription'] .  "<td>";
+                echo "<td>" . $products[$x]['PPrice'] . "<td>";
+                echo "</tr>";
+                
+            }
+        ?>
+        </table>
+	</body>
+</html>
