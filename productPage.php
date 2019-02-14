4678e64befc8c834dc1c6a50c7521d3bc77f9c3a
Charles Henderson <chenderson33@my.gcu.edu>
2019-02-14 09:13:10 -0700

    attempting random internet methods now

--- productPage.php
+++ productPage.php
@@ -60,8 +60,9 @@ error_reporting(E_ALL);
 				    echo "<td>" . $product_array[$x]['PPrice'] . "<td>";
 				    print "<td>" . $picture_array[$x]['IMG'] . "<td>";
 				    ?>
+				    <td>
 				    <img src="data:image/jpg;base64,<?php echo base64_encode($picture_array[$x]['IMG']); ?>" class="latest_images img-responsive img-thumbnail" alt="Cinque Terre" >
-				    
+				    </td>
 		    		<?php 
 				    for($y = 0; $y < count($picture_array); $y++){
 				        if($product_array[$x]['PDescription'] == $picture_array[$y]['PicDescription'])
