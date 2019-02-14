e036284b3da075ef9dc7139d90ca48aabce0949d
Charles Henderson <chenderson33@my.gcu.edu>
2019-02-14 02:19:14 -0700

    forcing harder

--- productPage.php
+++ productPage.php
@@ -58,7 +58,7 @@ error_reporting(E_ALL);
 				    echo "<td>" . $product_array[$x]['PName'] . "<td>";
 				    echo "<td>" . $product_array[$x]['PDescription'] .  "<td>";
 				    echo "<td>" . $product_array[$x]['PPrice'] . "<td>";
-				    echo [$picture_array][$x]['IMG'];
+				    echo "<td>" . $picture_array[$x]['IMG'] . "<td>";
 				    ?>
 				    
 				    
