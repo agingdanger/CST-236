793fe01d6f32e2d2be11c9ef8afcffafe491dabd
TheSmartDeveloper <kgsmart95@gmail.com>
2019-03-06 22:34:38 -0700

    updated the style of the _displaySearchResults.php to match the theme

--- _displaySearchResults.php
+++ _displaySearchResults.php
@@ -3,10 +3,11 @@
 		<meta charset="UTF-8"> <!-- text encoding set up-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--viewport defined to scale for all devices-->
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"> <!--backwards compatabillity with IE/edge products-->
-		<title> Login </title> <!-- tab title display -->
+		<title> Product Results </title> <!-- tab title display -->
+        <link rel="stylesheet" href="loginStyle.css"> <!-- style sheet being pulled from -->
 	</head>
 	<body>
-		<table class="ttext">
+		<table class="ttext" style="width:100%">
     			<tr>
         			<td>
         				ID
@@ -20,6 +21,11 @@
         			<td>
         				Product Price
         			</td>
+                    <td>
+                        Product Info
+                    </td>
+                </tr>
+                <tr>
         				
 		<?php 
             // Expects an array of $person. Display the results in a table.
@@ -38,17 +44,16 @@
                 
                 
                 ?>
-                <td> Product Info
-                <form action="productPage.php" method = "POST">
-                <input type = "hidden" name = "id" value = " <?php echo $products[$x]['PID'] ?> "></input>
-                <button type = "submit">Details</button>
-                </form>
-                <form action="shoppingCart.php" method = "POST">
-                <input type = "hidden" name = "id" value = " <?php echo $products[$x]['PID'] ?> "></input>
-                <button type = "submit">Add to Cart</button>
-                </form>
-                </td>
-                	
+                    <td>
+                    <form action="productPage.php" method = "POST">
+                        <input type = "hidden" name = "id" value = " <?php echo $products[$x]['PID'] ?> "></input>
+                        <button type = "submit" class="button">Details</button>
+                    </form>
+                    <form action="shoppingCart.php" method = "POST">
+                        <input type = "hidden" name = "id" value = " <?php echo $products[$x]['PID'] ?> "></input>
+                        <button type = "submit" class="button">Add to Cart</button>
+                    </form>
+                    </td>
                 <?php 
                 echo "</tr>";
             }
--- searchHandler.php
+++ searchHandler.php
@@ -1,30 +1,38 @@
 <?php 
 
-require_once 'productService.php';
+    require_once 'productService.php';
 
-$searchPhrase = $_POST['product'];
+    $searchPhrase = $_POST['product'];
 
-$p = new productService();
+    $p = new productService();
 
-
-
-$products = $p->findProducts($searchPhrase);
-
-
-
+    $products = $p->findProducts($searchPhrase);
 ?>
-<h2>Search Results</h2>
-<p>Here is what we found</p>
+<html lang ="en">
+	<head>
+		<meta charset="UTF-8"> <!-- text encoding set up-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--viewport defined to scale for all devices-->
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"> <!--backwards compatabillity with IE/edge products-->
+		<title> Login </title> <!-- tab title display -->
+		<link rel="stylesheet" href="loginStyle.css"> <!-- style sheet being pulled from -->
+	</head>
+    <body>
+    <h2 class="ttext">Search Results</h2>
+    <!--<p class="ptext">Here is what we found</p>-->
+    </body>
+</html>
 
 <?php
 
-if($products){
-    //we got some results
-    include("_displaySearchResults.php");
-}
-else{
-   echo "Nobody found here<br>";
-}
+    if($products)
+    {
+        //we got some results
+        include("_displaySearchResults.php");
+    }
+    else
+    {
+    echo "Nobody found here<br>";
+    }
 
 
 ?>
