7bbb5f41c09b065a6eea380de84c1e494f357e2d
Charles Henderson <chenderson33@my.gcu.edu>
2019-03-16 23:55:30 -0700

    tablated the ordered item, still needs to be written to contain multiple cart items

--- shoppingCart.php
+++ shoppingCart.php
@@ -1,3 +1,12 @@
+ <html>
+	<head>
+		<meta charset="UTF-8"> <!-- text encoding set up-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--viewport defined to scale for all devices-->
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"> <!--backwards compatabillity with IE/edge products-->
+		<title> Product Results </title> <!-- tab title display -->
+        <link rel="stylesheet" href="loginStyle.css"> <!-- style sheet being pulled from -->
+	</head>
+	<body>
  <?php
  require_once 'db_connector.php';
  
@@ -17,12 +26,9 @@
 
  $sql_statement = "INSERT INTO l426moc0o088s6g9.Order (userID) VALUES ('$id')";
  $result = mysqli_query($connection, $sql_statement);
- /*if($result){
-     echo "We did it";
+ if($result){
+
  }
- else{
-     echo "Dangit";
- }*/
  
  //echo $sql_statement;
  
@@ -34,14 +40,52 @@
         $orderID =  $_SESSION['orderID'] = $row['OID'];  
      }
  }
- echo $pid . "<br>";
- echo $quantity . "<br>";
- echo $orderID . "<br>";
  
  $insert = "INSERT INTO l426moc0o088s6g9.`Order List` (productID, PQuantity, orderID) VALUES('$pid', '$quantity', '$orderID')";
- $result3 = mysqli_query($connection, $insert);
- if($result3){
-     echo "We did it again";
+ $pastorders = "SELECT * FROM l426moc0o088s6g9.`Order List` WHERE OLID = LAST_INSERT_ID()";
+ $result3 = $connection->query($insert);
+
+ $pastorderquery = $connection->query($pastorders);
+ 
+ if($pastorderquery){
+
+    
+        $cart_array = array();
+        
+        while($order = $pastorderquery->fetch_assoc()){
+            array_push($cart_array,$order);
+        }
+    
+        if($cart_array){
+            ?>
+            <table class="ttext" style="width:100%">
+    			<tr>
+        			<td>
+        				Order List Row ID
+        			</td>
+        			<td>
+        				Product ID
+        			</td>
+        			<td>
+        				Product Quantity
+        			</td>
+        			<td>
+        				Order ID
+        			</td>
+                </tr>
+                
+            <?php 
+            for($x = 0; $x < count($cart_array); $x++)
+            {
+                //echo "<tr>";
+                echo "<td>" . $cart_array[$x]['OLID'] . "</td>";
+                echo "<td>" . $cart_array[$x]['productID'] . "</td>";
+                echo "<td>" . $cart_array[$x]['PQuantity'] .  "</td>";
+                echo "<td>" . $cart_array[$x]['orderID'] . "</td>";
+            }
+                
+            
+        }
  }
  else{
      echo "Try more";
@@ -53,4 +97,11 @@
  
  //   echo "will be overwritten";
 ?>
- <img src="https://thumbs.gfycat.com/CharmingIdioticBlobfish-size_restricted.gif" alt="The Simpsons" style="width:700px;height:393px;"> 
+        <!--</tr>	-->
+        </table>
+          <img src="https://thumbs.gfycat.com/CharmingIdioticBlobfish-size_restricted.gif" alt="The Simpsons" style="width:700px;height:393px;"> 
+	</body>
+</html>
+<?php
+
+?>
