c0f04e691e778f36e7c304fbd6770d750f5c8441
SkieXI <43129232+SkieXI@users.noreply.github.com>
2019-04-01 15:51:00 -0700

    Here we are
    Yet again!

--- shoppingCart.php
+++ shoppingCart.php
@@ -14,7 +14,7 @@
  ini_set('display_errors', 1);
  ini_set('display_startup_errors', 1);
  error_reporting(E_ALL);
- session_start();
+ //session_start();
  
  $id = $_SESSION['userID'];
  //echo $id;
